e6afd6b7c8c70e692aee79e73ad28f27866709e8
kwanghoon.kim <kk03187693@gmail.com>
2021-01-03 21:09:04 +0900

    add repository example

--- src/main/java/study/datajpa/Repository/MemberRepository.java
+++ src/main/java/study/datajpa/Repository/MemberRepository.java
@@ -1,7 +1,11 @@
 package study.datajpa.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     /*
@@ -11,5 +15,14 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     * memberRepository.getClass() class com.sun.proxy.$ProxyXXX
     */
 
+    /* 쿼리 메소드 제공 */
+    List<Member> findByUsername(String username);
 
+    /* 이름 기반 파라미터 바인딩 -> 위치 기반은 잘 사용하지 않음 */
+    @Query("select m from Member m where m.username = :name")
+    Member findMembers(@Param("name") String username);
+
+    /* 컬렉션 파라미터 바인딩 -> in 절 사용 */
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String> names);
 }
